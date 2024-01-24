52217b6786527ef0cf2c255541b990b17fb20f0b
Felix <felix.chung@felixchung.net>
2024-01-24 16:51:25 +0800

    update chart path

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "datahub" {
   name       = "datahub"
-  chart      = "/chart/charts/datahub"
+  chart      = "/chart"
   namespace  = "datahub-ns"
   create_namespace = true
 }
